11b3bec0ca6cd3f90f443f26ee57f91bdb042503
Fellype <fellype.amorim88@gmail.com>
2020-08-31 11:56:02 -0300

    Criado um sistema utilizando uma excessao de forma errada

---
+++ ResolvidoExcessoesPersonalizadas/Program.cs
@@ -0,0 +1,53 @@
+﻿using ResolvidoExcessoesPersonalizadas.Entities;
+using System;
+
+namespace ResolvidoExcessoesPersonalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int roomnumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+            if(checkout <= checkin)
+            {
+                Console.WriteLine("Error in reservation: check-out date must be after check-in");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(roomnumber, checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+
+                if (checkin < now || checkout < now)
+                {
+                    Console.WriteLine("Error in reservation: Reservation dates must be future dates");
+                }
+                else if (checkout <= checkin)
+                {
+                    Console.WriteLine("Error in reservation: check-out date must be after check-in");
+                }
+                else
+                {
+                    reservation.UpdateDates(checkin, checkout);
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+            }
+            
+
+            
+        }
+    }
+}
